ff8c08e4a8d24f75bb611d55789a840cc5baf91c
demo-college <d15784889@gmail.com>
2023-08-08 18:24:07 +0000

    commit

---
+++ angularapp/src/index.py
@@ -0,0 +1,395 @@
+print("hiii byee ")
+
+
+
+
+
+
+for i in range(1000):
+    print("Bhg")
+
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
+for i in range(1000):
+    print("Bhg")
+
